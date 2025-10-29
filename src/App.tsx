08f9c7f62ce2c2744db1cffac54c85d875e652e3
Dewni Amaya Jayasekara <153248798+selya1722@users.noreply.github.com>
2025-10-29 18:42:36 +0530

    Add new routes for Projects, SheSafe, TFS, and Aloe

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
+import Projects from "@/components/Projects";
+import SheSafe from "./pages/shesafe";
+import TFS from "./pages/tfs";  
 import NotFound from "./pages/NotFound";
+import Aloe from "./pages/aloe";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +21,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/shesafe" element={<SheSafe />} />
+          <Route path="/tfs" element={<TFS />} />
+          <Route path="/aloe" element={<Aloe />} />
+          {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
